2708be484321caeab796c990aa831c53420dcc7a
winniezhengg <winnie.zhengg@gmail.com>
2021-03-15 12:00:00 -0400

    Task #40: add search bar - no functionality

--- app/src/main/java/com/example/nottingham/MainActivity.java
+++ app/src/main/java/com/example/nottingham/MainActivity.java
@@ -6,12 +6,21 @@ import android.content.Intent;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.SearchView;
 import android.widget.TextView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     private Button quick_access;
+    ArrayAdapter<String> adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,10 +35,40 @@ public class MainActivity extends AppCompatActivity {
                 openQuickAccess();
             }
         });
+
+        List<String> mylist = new ArrayList<>();
+        mylist.add("one");
+        mylist.add("two");
+        mylist.add("three");
+        mylist.add("four");
+        adapter = new ArrayAdapter<>(this,android.R.layout.simple_list_item_1,mylist);
+        
+
     }
 
     public void openQuickAccess(){
         Intent intent  = new Intent(this, quick_access.class);
         startActivity(intent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_home,menu);
+        MenuItem menuItem = menu.findItem(R.id.search_home);
+        SearchView searchView = (SearchView) menuItem.getActionView();
+        searchView.setQueryHint("Search here");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                adapter.getFilter().filter(newText);
+                return true;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
     }
 }
