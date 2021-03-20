3f49235ab5407f65beadee0de9231342c0c9794f
Jiaqi Yang <jiaqiyang@jiaqis-mbp.home>
2021-03-19 23:35:17 -0400

    Task #55:Set up the frontend for the price trend figure

--- app/src/main/java/com/example/nottingham/real_time.java
+++ app/src/main/java/com/example/nottingham/real_time.java
@@ -7,12 +7,13 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class real_time extends AppCompatActivity {
     private Button backHome;
     TextView realTimePrice;
-
+    ImageView currentPriceTrend;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,7 +25,10 @@ public class real_time extends AppCompatActivity {
         backHome.setOnClickListener(v -> openHome());
 
         // Display Stock price
-        realTimePrice = findViewById(R.id.realTimePrice);
+        realTimePrice = (TextView) findViewById(R.id.realTimePrice);
+
+        // Display Stock Trend
+        currentPriceTrend = (ImageView) findViewById(R.id.currentPriceTrend);
     }
 
     public void openHome(){
@@ -32,15 +36,17 @@ public class real_time extends AppCompatActivity {
             startActivity(intent);
     }
 
-    // get the price from API and show it from the TextView "realTimePrice"
+    // Get the price from API and show it from the TextView "realTimePrice"
     public void showPrice(){
         String price = "$0.0";
         realTimePrice.setText(price);
 
     }
 
+    //Get the price list from API and return it as a image to the real page
+    public void insertTrend() {
 
-
+    }
 
 
 }
