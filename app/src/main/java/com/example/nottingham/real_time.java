e96f0289c25bfb17b91cd22a8f571b5023f825b9
Jiaqi Yang <jiaqiyang@jiaqis-mbp.home>
2021-03-19 22:51:20 -0400

    Task #54:Set up the frontend for the price, include the property of the variable returned from backend implementation.

--- app/src/main/java/com/example/nottingham/real_time.java
+++ app/src/main/java/com/example/nottingham/real_time.java
@@ -7,22 +7,40 @@ import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class real_time extends AppCompatActivity {
     private Button backHome;
+    TextView realTimePrice;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_real_time);
-
+        // Back Button implementation
         backHome = (Button)findViewById(R.id.backHome);
         backHome.setBackgroundColor(Color.WHITE);
         backHome.setOnClickListener(v -> openHome());
+
+        // Display Stock price
+        realTimePrice = findViewById(R.id.realTimePrice);
     }
 
     public void openHome(){
             Intent intent  = new Intent(this, MainActivity.class);
             startActivity(intent);
-        }
+    }
+
+    // get the price from API and show it from the TextView "realTimePrice"
+    public void showPrice(){
+        String price = "$0.0";
+        realTimePrice.setText(price);
+
+    }
+
+
+
+
+
 }
