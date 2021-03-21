a7c71cadda3387d6ea2c21e7bc89903c1739338f
Steve <Steveglennjoseph@gmail.com>
2021-03-21 17:09:05 -0400

    Task #45: Implement Price and Plot of current price trend on the real time page

--- app/src/main/java/com/example/nottingham/real_time.java
+++ app/src/main/java/com/example/nottingham/real_time.java
@@ -3,12 +3,18 @@ package com.example.nottingham;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import com.chaquo.python.PyObject;
+import com.chaquo.python.Python;
+import com.chaquo.python.android.AndroidPlatform;
 
 public class real_time extends AppCompatActivity {
     private Button backHome;
@@ -27,9 +33,26 @@ public class real_time extends AppCompatActivity {
 
         // Display Stock price
         realTimePrice = (TextView) findViewById(R.id.realTimePrice);
+            //Using chaquopy and script
+        if (!Python.isStarted()) {
+            Python.start(new AndroidPlatform(this));
+        }
+        Python python = Python.getInstance();
+        PyObject pythonFile = python.getModule("RealTimePython");
+        PyObject helloWorldString = pythonFile.callAttr("getPrice","AAPL");
+        realTimePrice.setText(helloWorldString.toString());
+
+
 
         // Display Stock Trend
         currentPriceTrend = (ImageView) findViewById(R.id.currentPriceTrend);
+       PyObject frame = pythonFile.callAttr("Plotter","AAPL","2021-02-20");
+        byte[] frameData = python.getBuiltins().callAttr("bytes", frame).toJava(byte[].class);
+        Bitmap bitmap = BitmapFactory.decodeByteArray(frameData, 0, frameData.length);
+        Bitmap bMapScaled = Bitmap.createScaledBitmap(bitmap, 1500, 1500, true);
+        currentPriceTrend.setImageBitmap(bMapScaled);
+
+
     }
 
     public void openHome(){
