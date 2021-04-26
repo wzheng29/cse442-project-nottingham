9980da3bc6e910079fba74486ec056d7e8bca2d6
winniezhengg <winnie.zhengg@gmail.com>
2021-04-25 22:13:06 -0400

    Task #61: Login page/ activity Added two EditText element for username and password. Added two button, one for login and one for registering. Added a temp home button for testing.

---
+++ app/src/main/java/com/example/nottingham/login.java
@@ -0,0 +1,28 @@
+package com.example.nottingham;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class login extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Button home;
+        home = (Button)findViewById(R.id.home_button);
+        home.setBackgroundColor(Color.WHITE);
+        home.setOnClickListener(v -> openHome());
+
+    }
+    public void openHome(){
+        Intent intent  = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
+}
